2f84a618602416e397fb73936949b48b80f19d1f
Nontoberks <nontobeko@bmglobal.co.za>
2021-08-20 17:27:50 +0200

    first commit

---
+++ ReceiveMessage/Program.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace ReceiveMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set up the factory
+            var factory = new ConnectionFactory() { HostName = "localHost" };
+
+            // Factory connection
+            using (var connection = factory.CreateConnection())
+
+            // New channel for pushing the message
+            using (var channel = connection.CreateModel())
+            {
+                // Queue properties
+                channel.QueueDeclare(queue: "messageSaver",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+                // This section is the consumer section that contains asynchronous method. This where the channel is consumed
+                var consumer = new EventingBasicConsumer(channel);
+
+                // The event collects the data/message
+                consumer.Received += (model, ea) => // this is an event. ea is an argument
+                {
+                    // This event  Handler handles asynchronous delivery
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine("------------------------ MESSAGE RECEIVED -------------------------");
+                    Console.WriteLine("{0}", message);
+
+                    // Extract name from the string 
+                    string sendersName = message.Substring(message.IndexOf(",") + 2);
+
+                    // Output Message
+                    Console.WriteLine("\n------------------- RESPONSE TO RECEIVED MESSAGE -------------------");
+                    Console.WriteLine($"Hello {sendersName}, I am your father");
+                    Console.WriteLine("\nPress [enter] to exit");
+
+                    Console.WriteLine();
+                };
+                // Consume the data
+                channel.BasicConsume("messageSaver",
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
